f11466d0110a49444c2100d7ad49feccf3111d7c
tangweikun <819573105@qq.com>
2018-06-13 11:19:13 +0800

    :heavy_plus_sign: Add koa-router

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 const app = new Koa()
+const router = new Router()
 
-// response
-app.use(ctx => {
-  ctx.body = 'Hello Koa'
-})
+router
+  .get('/foo', (ctx, next) => {
+    ctx.body = 'foo'
+  })
+  .get('/bar', (ctx, next) => {
+    ctx.body = 'bar'
+  })
+
+app.use(router.routes())
 
 app.listen(3000)
 console.log('listen port 3000')
